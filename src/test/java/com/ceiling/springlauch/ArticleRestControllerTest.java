a7e0e4051cc40b08f0ad344e4bcadb51c232c229
WuBingrui <student_wbr@163.com>
2020-05-18 19:23:11 +0800

    Mock http请求

---
+++ src/test/java/com/ceiling/springlauch/ArticleRestControllerTest.java
@@ -0,0 +1,53 @@
+package com.ceiling.springlauch;
+
+import com.ceiling.springlauch.controller.ArticleRestController;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@Slf4j
+@SpringBootTest
+public class ArticleRestControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp(){
+        mockMvc = MockMvcBuilders.standaloneSetup(new ArticleRestController()).build();
+    }
+
+    @Test
+    public void saveArticle() throws Exception{
+        String article = "{\n" +
+                "    \"id\": 1,\n" +
+                "    \"author\": \"aaa\",\n" +
+                "    \"title\": \"spring boot\",\n" +
+                "    \"content\": \"c\",\n" +
+                "    \"createTime\": \"2020-07-16 05:23:34\",\n" +
+                "    \"reader\":[{\"name\":\"aaa\",\"age\":18},{\"name\":\"bbb \",\"age\":37}]\n" +
+                "}";
+
+        MvcResult result = mockMvc.perform(
+                MockMvcRequestBuilders.request(HttpMethod.POST,"/rest/article")
+                        .contentType("application/json").content(article))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$data.author").value("aaa"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$data.reader[0].age").value(18))
+                .andDo(print())
+                .andReturn();
+
+        log.info(result.getResponse().getContentAsString());
+
+    }
+}
